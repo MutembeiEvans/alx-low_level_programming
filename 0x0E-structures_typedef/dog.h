8c98e658eb3ee6e649de25bd4fb2d87b4669222f
MutembeiEvans <evansmutembeinjeru@gmail.com>
2022-05-12 16:45:16 +0300

    Task 0, struct dog with given elements

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,26 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Conatins details of dog
+ * @name: dog name
+ * @age: dog age
+ * @owner:  owner name
+ */
+
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+
+void init_dog(struct dog *D, char *name, float age, char *owner);
+void print_dog(struct dog *D);
+/**
+ * dog - rename struct with typedef
+ */
+typedef struct dog my_dog;
+my_dog *new_dog(char *name, char *owner, float age);
+void free_dog(my_dog *d);
+#endif
